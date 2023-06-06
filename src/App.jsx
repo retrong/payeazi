21e5b548e841dc0183de74aaaf8dd8dd96ce36e7
retrong <eguobabarth@gmail.com>
2023-06-06 07:34:27 +0100

    implement landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Button } from './components/UI/atoms/Button';
+import LandingPage from './components/pages/landingPage.jsx';
 
 export default function App() {
 	return (
-		<div>
-			<Button>my button</Button>
-		</div>
+       <>
+       <LandingPage className='text-sky-400'>
+        <h1 className='text-gray-50'>Payeazi</h1>
+       </LandingPage>
+       </>
+		
 	);
 }
--- src/components/pages/landingPage.jsx
+++ src/components/pages/landingPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Button } from '../UI/atoms/Button';
 
 const landingPage = () => {
-	return <div>landingPage</div>;
+	return (
+		<>
+			<nav className="bg-black ">
+				<ul className='flex flex-row text-sky-400'>
+					<li>Home</li>
+					<li>About</li>
+					<li>Feature</li>
+				</ul>
+				<Button className="rounded" />
+			</nav>
+		</>
+	);
 };
 
 export default landingPage;
