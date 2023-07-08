59977daea76200902ea40a2f913815d3b0ff1b87
retrong <eguobabarth@gmail.com>
2023-07-09 00:25:09 +0100

    implement featureCard component

--- src/components/UI/molecules/Feature.jsx
+++ src/components/UI/molecules/Feature.jsx
@@ -1,12 +1,29 @@
-import styles from '../../style'
+import { features } from '../../../constants';
+import styles from '../../style';
 
-const Feature = () => (
-    <div className={`flex flex-1 ${styles.flexCenter} ${styles.paddingX} mb-5 relative bottom-56`}>
-        <div className={`flex bg-white rounded-2xl w-full h-[200px] px-0 sm:px-14 py-4`}>
-
+const FeatureCard = ({icon, title, content, index}) => (
+    <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "bg-6" : "mb-0"} ${styles.flexCenter} ${styles.paddingX} mb-5 relative bottom-56`}>
+        <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue flex px-0 sm:px-14 py-4`}>
+            <img src={icon} alt="icon" />
+        </div>
+        <div className='flex flex-1 flex-col ml-3'>
+            <h4>
+                {title}
+            </h4>
+            <p>
+                {content}
+            </p>
         </div>
     </div>
 )
 
+// const Business = () => {
+//     return (
+//         <section>
+            
+//         </section>
+//     )
+// }
 
-export default Feature
+
+export default FeatureCard
