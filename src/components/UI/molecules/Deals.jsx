299f15cd9352e24a96bc51e45df6b884122d9017
retrong <eguobabarth@gmail.com>
2023-07-21 00:09:45 +0100

    modified styling in deals component

--- src/components/UI/molecules/Deals.jsx
+++ src/components/UI/molecules/Deals.jsx
@@ -5,23 +5,29 @@ import GetStarted from '../atoms/GetStarted';
 
 const Deals = () => {
   return (
-    <section className={`${layout.section}`}>
-        <div className={`${layout.sectionInfo}`}>
-            <img src={worldMap} alt="world-map" className='w-[100%] h-[100%] z-[5]'/>
-        </div>
-
-        <div>
-            <h2 className={`${styles.heading2}`}>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, ipsum.
-            </h2>
-            <p className={`${styles.paragraph}`}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, quisquam quis? Recusandae, quo enim aspernatur blanditiis dicta voluptas quisquam perferendis? 
-            </p>
-            <GetStarted />
-
-        </div>
-    </section>
-  )
+		<section
+			className={`${layout.section} relative top-[-200px] max-md:top-[0px] max-sm:mt-2`}
+		>
+			<div className={`${layout.sectionInfo}`}>
+				<h2 className={`${styles.heading2}`}>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+				</h2>
+				<p className={`${styles.paragraph}`}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
+					quisquam quis? Recusandae, quo enim aspernatur blanditiis dicta
+					voluptas quisquam perferendis?
+				</p>
+				<GetStarted />
+			</div>
+			<div className={`${layout.sectionImg}`}>
+				<img
+					src={worldMap}
+					alt="worldmap"
+					className="w-[100%] h-[100%] z-[5]"
+				/>
+			</div>
+		</section>
+	);
 }
 
 export default Deals
