3c114e01926c5502f6deccf472524d67d0679e76
retrong <eguobabarth@gmail.com>
2023-07-28 08:27:46 +0100

    modified styling in cta component

--- src/components/UI/molecules/Cta.jsx
+++ src/components/UI/molecules/Cta.jsx
@@ -4,13 +4,13 @@ import GetStarted from "../atoms/GetStarted.jsx";
 
 const Cta = () => {
   return (
-    <section className={`${styles.padding} ${styles.flexCenter} bg-[#f9fbdf] rounded-[24px] mb-5`}>
-      <div className='mr-5'>
+    <section className={`${styles.padding} ${styles.flexCenter} flex-wrap bg-[#f9fbdf] rounded-[24px] mb-5 relative top-[-200px] max-md:top-[0px] max-sm:mt-2`}>
+      <div>
         <h2 className={`font-cabin font-semibold xs:text-[52px] text-[42px] text-black xs:leading-[76.8px] leading-[66.8px] w-full`}>Start Making Payment Now </h2>
         <p className={`font-cabin font-normal text-black text-[20px] leading-[28px] max-w-[470px] mt-2`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
 
-      <div>
+      <div className={`ml-0 sm:ml-8`}>
         <GetStarted />
       </div>
     </section>
