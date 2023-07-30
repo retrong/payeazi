60189df3c99e083f1cf463c20d5da74b83cc4033
retrong <eguobabarth@gmail.com>
2023-07-30 02:18:59 +0100

    fixed error in depolment

---
+++ src/components/pages/LandingPage.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { orange } from '../../assets';
+import Cta from "../UI/molecules/Cta.jsx";
+import Deals from '../UI/molecules/Deals';
+import Feature from '../UI/molecules/Feature';
+import Footer from "../UI/molecules/Footer.jsx";
+import Hero from '../UI/molecules/Hero';
+import Navbar from '../UI/molecules/Navbar';
+import Product from '../UI/molecules/Product';
+import Stats from '../UI/molecules/Stats';
+import Testimonies from '../UI/molecules/Testimonies';
+import styles from '../style';
+
+const LandingPage = () => (
+	<section className="bg_gradient w-full overflow-hidden h-15 shadow">
+		<div className={`${styles.paddingX} ${styles.flexCenter} mx-5 sm:mx-0`}>
+			<div className={`${styles.boxWidth}`}>
+				<Navbar />
+			</div>
+		</div>
+		<div className={`${styles.flexStart} ${styles.marginX}`}>
+			<div className={`${styles.boxWidth}`}>
+				<Hero />
+			</div>
+		</div>
+        
+        <div className='z-[-5]'>
+            <img src={orange} alt="orange" className="w-[50%] h-[20%] max-md:hidden relative bottom-28 right-44" />
+        </div>
+
+		<div className={`${styles.flexStart} ${styles.paddingX}`}>
+			<div className={`${styles.boxWidth}`}>
+                <Stats />
+				<Feature />
+                <Product/>
+                <Deals />
+                <Testimonies />
+				<Cta />
+			</div>
+		</div>
+
+		<div className={`bg-black ${styles.paddingX}`}>
+			<div className={`${styles.boxWidth}`}>
+				<Footer />
+			</div>
+		</div>
+	</section>
+);
+
+
+export default LandingPage;
